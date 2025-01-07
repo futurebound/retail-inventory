c9c45f0f88641870782d8bffc0450995bbd7b553
futurebound <mitchellm1618@gmail.com>
2025-01-07 13:16:45 -0800

    confirm running locally, accessing .env PORT

---
+++ app.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`)
+})
